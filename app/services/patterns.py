24e406162c0f510b20058b1e34dfaa4e23662303
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-21 13:46:09 +0500

    add email pattern

--- app/services/patterns.py
+++ app/services/patterns.py
@@ -2,7 +2,12 @@ from re import compile
 
 
 text_pattern = compile(r"[a-zA-Z0-9]")
+email_pattern = compile(r"\w+@\w+.\w+")
 
 
 def check_text(text: str) -> bool:
     return bool(text_pattern.findall(text))
+
+
+def check_email(email: str) -> bool:
+    return email_pattern.fullmatch(email) is not None
