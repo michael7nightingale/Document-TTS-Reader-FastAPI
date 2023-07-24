fdbba7357385dbc0cf4339c8b30291dfb3be5000
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-24 14:39:56 +0500

    create main tests

---
+++ tests/tests_api/test_main.py
@@ -0,0 +1,10 @@
+import pytest
+from fastapi import status
+
+from tests_api.conftest import get_main_url
+
+
+class TestMain:
+    async def test_main_url(self, client):
+        response = await client.get(get_main_url("homepage"))
+        assert response.status_code == status.HTTP_200_OK
