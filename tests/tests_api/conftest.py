59d6e4a71027c87551e061e20f609fda13d5d9b5
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-25 12:22:48 +0500

    create test data

--- tests/tests_api/conftest.py
+++ tests/tests_api/conftest.py
@@ -1,19 +1,26 @@
+import os.path
+from shutil import rmtree
+from uuid import uuid4
+
 import pytest_asyncio
 from httpx import AsyncClient
+from fastapi import FastAPI, APIRouter
 
 from app.api.routes import (
     main_router,
     documents_router,
-    translator_router,
-    auth_router
+    auth_router,
+
 )
 from app.core.server import Server
 from app.db import Base
-from app.db.repositories import UserRepository
+from app.db.repositories import UserRepository, DocumentRepository
+from app.services.documents import get_pages_count_and_cover
+from app.services import get_name_and_extension
 
 
 @pytest_asyncio.fixture
-async def app():
+async def app() -> FastAPI:
     server = Server(test=True)
     async with server.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
@@ -23,7 +30,7 @@ async def app():
 
 
 @pytest_asyncio.fixture
-async def client(app):
+async def client(app: FastAPI) -> AsyncClient:
     async with AsyncClient(app=app, base_url="http://localhost:8000/api/v1/") as client_:
         yield client_
 
@@ -47,7 +54,23 @@ def user2():
 
 
 @pytest_asyncio.fixture
-async def authorized_client(client: AsyncClient, users_test_data, user2):
+async def client_user1(client: AsyncClient, users_test_data: dict, user1: dict):
+    user_token_data = {
+        "username": user1['username'],
+        "password": user1['password']
+    }
+    response = await client.post(
+        get_auth_url("get_token"),
+        json=user_token_data
+    )
+    access_token = response.json()["access_token"]
+    headers = {"Authorization": f"Bearer {access_token}"}
+    client.headers = headers
+    yield client
+
+
+@pytest_asyncio.fixture
+async def client_user2(client: AsyncClient, users_test_data: dict, user2: dict):
     user_token_data = {
         "username": user2['username'],
         "password": user2['password']
@@ -63,13 +86,59 @@ async def authorized_client(client: AsyncClient, users_test_data, user2):
 
 
 @pytest_asyncio.fixture
-async def users_test_data(app, user1, user2):
+async def users_test_data(app: FastAPI, user1: dict, user2: dict):
     async with app.state.pool() as session:
         users_repo = UserRepository(session)
         user1_ = await users_repo.register(user1)
         await users_repo.activate(user1_.id, user1_.email)
         user2_ = await users_repo.register(user2)
         await users_repo.activate(user2_.id, user2_.email)
+        users = user1_, user2_
+        yield users
+        users_paths = tuple((os.path.join(app.state.STATIC_DIR, user.id) for user in users))
+        for path in users_paths:
+            if os.path.exists(path):
+                rmtree(path)
+
+
+@pytest_asyncio.fixture
+async def documents_test_data(app: FastAPI, users_test_data: tuple):
+    user1, user2 = users_test_data
+    user1_path = user1.id
+    user1_fullpath = os.path.join(app.state.STATIC_DIR, user1_path)
+    user2_path = user1.id
+    user2_fullpath = os.path.join(app.state.STATIC_DIR, user2_path)
+    if not os.path.exists(user1_fullpath):
+        os.makedirs(user1_fullpath)
+    if not os.path.exists(user2_fullpath):
+        os.makedirs(user2_fullpath)
+    async with app.state.pool() as session:
+        documents_repo = DocumentRepository(session)
+        with open("tests/tests_api/files/python.pdf", "rb") as file_to_read:
+            document1_uuid = str(uuid4())
+            filename, extension = get_name_and_extension("python.pdf")
+            document1_path = os.path.join(user1_path, document1_uuid)
+            document1_fullpath = os.path.join(app.state.STATIC_DIR, document1_path)
+            os.makedirs(document1_fullpath)
+            document_file_path = os.path.join(document1_path, f"{filename}.{extension}")
+            document_file_fullpath = os.path.join(app.state.STATIC_DIR, document_file_path)
+            cover_path = os.path.join(document1_path, f"{filename}_cover.png")
+            cover_fullpath = os.path.join(app.state.STATIC_DIR, cover_path)
+            with open(document_file_fullpath, "wb") as file_to_write:
+                file_to_write.write(file_to_read.read())
+
+            pages = get_pages_count_and_cover(extension, document_file_fullpath, cover_fullpath)
+            document1 = await documents_repo.create(
+                id=document1_uuid,
+                title=filename,
+                pages=pages,
+                current_page=0,
+                document_url=document_file_path,
+                cover_url=cover_path,
+                user_id=user1.id,
+                extension=extension
+            )
+    yield document1
 
 
 @pytest_asyncio.fixture
@@ -82,20 +151,19 @@ def user_not_activated():
 
 
 @pytest_asyncio.fixture
-async def not_active_user(app, user_not_activated):
+async def not_active_user(app: FastAPI, user_not_activated: dict):
     async with app.state.pool() as session:
         user_repo = UserRepository(session)
         user = await user_repo.register(user_not_activated)
         yield user
 
 
-def url_for(router):
-    def inner(name, **kwargs):
+def url_for(router: APIRouter):
+    def inner(name: str, **kwargs) -> str:
         return router.url_path_for(name, **kwargs)
     return inner
 
 
 get_main_url = url_for(main_router)
 get_auth_url = url_for(auth_router)
-get_translator_url = url_for(translator_router)
 get_document_url = url_for(documents_router)
